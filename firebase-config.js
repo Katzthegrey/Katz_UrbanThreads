5854e5f2213f141dd7281bfabb14da36817cb588
Katzthegrey <katzkatlego310@gmail.com>
2026-04-24 22:00:33 +0200

    Add firebase-config.js to gitignore

--- firebase-config.js
+++ firebase-config.js
@@ -4,17 +4,18 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-// Read config from environment variables
+// Use window._env_ for Netlify injection (works in browser)
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: window._env_?.FIREBASE_API_KEY,
+  authDomain: window._env_?.FIREBASE_AUTH_DOMAIN,
+  projectId: window._env_?.FIREBASE_PROJECT_ID,
+  storageBucket: window._env_?.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: window._env_?.FIREBASE_MESSAGING_SENDER_ID,
+  appId: window._env_?.FIREBASE_APP_ID,
+  measurementId: window._env_?.FIREBASE_MEASUREMENT_ID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
